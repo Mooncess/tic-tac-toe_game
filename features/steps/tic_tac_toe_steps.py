aca401206c76659687f3477e2b5f76fe56e54191
Mooncess <shulik.2003@mail.ru>
2023-10-11 11:13:23 +0300

    написан авто-сценарий для проверки победы

---
+++ features/steps/tic_tac_toe_steps.py
@@ -0,0 +1,15 @@
+from behave import given, when, then
+
+
+@given('Пустое игровое поле')
+def step_given_empty_game_board(context):
+    context.board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+
+@when('Игрок выполнил условие выигрыша')
+def step_when_player_completes_winning_condition(context):
+    context.board = [["X", "O", "O"], ["O", "X", "O"], [" ", " ", "X"]]
+    context.player = "X"
+
+@then('Игра должна объявить игрока победителем')
+def step_then_game_should_declare_winner(context):
+    assert check_win(context.board, context.player) == True

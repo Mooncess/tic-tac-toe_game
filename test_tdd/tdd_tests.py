37678f28b8a9a4d90ca421a591bb0875857659b0
Hotway97 <Hotway97@yandex.ru>
2023-10-11 18:20:09 +0300

    написал tdd тест get_row

--- test_tdd/tdd_tests.py
+++ test_tdd/tdd_tests.py
@@ -2,7 +2,7 @@ import unittest
 from io import StringIO
 from unittest import mock
 
-from app.tic_tac_toe import print_board
+from app.tic_tac_toe import print_board, get_row
 
 
 class TicTacToeTest(unittest.TestCase):
@@ -26,6 +26,29 @@ class TicTacToeTest(unittest.TestCase):
 
         # Сравнение результатов
         self.assertEqual(captured_output.getvalue(), expected_output)
+
+    def test_get_row_valid_input(self):
+        """
+        Тестирование функции get_row с корректными данными,
+        которая возвращает номер строки,
+        введеный пользователем.
+        """
+        with mock.patch("builtins.input", side_effect=["1"]):
+            self.assertEqual(get_row("X"), 0)
+
+    def test_get_row_invalid_input(self):
+        """
+        Тестирование функции get_row с некорректными данными,
+        которая возвращает ошибку,
+        с просьбой попробовать снова.
+        """
+        with mock.patch("builtins.input", side_effect=["4", "2"]):
+            captured_output = StringIO()
+            with mock.patch("sys.stdout", captured_output):
+                row = get_row("O")
+            self.assertEqual(row, 1)
+            expected_output = "Некорректный ввод. Пробуйте снова.\n"
+            self.assertEqual(captured_output.getvalue(), expected_output)
 
 
 if __name__ == "__main__":

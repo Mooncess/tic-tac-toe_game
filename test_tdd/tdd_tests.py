62f1fc1d36bf5b1d0891987b2138fa62360c49a7
Mooncess <shulik.2003@mail.ru>
2023-10-11 10:50:32 +0300

    написан тест для функции print_board

---
+++ test_tdd/tdd_tests.py
@@ -0,0 +1,30 @@
+import unittest
+from io import StringIO
+from unittest import mock
+
+
+class TicTacToeTest(unittest.TestCase):
+
+    def test_print_board(self):
+        """
+        Тестирование функции print_board,
+        которая отвечает за вывод в консоль
+        игрового поля
+        """
+        # Переданная в функцию игровая доска
+        board = [["X", "O", "X"], ["O", "X", "O"], ["X", "O", "X"]]
+
+        # Ожидаемый результат
+        expected_output = "-------------\n| X | O | X | \n-------------\n| O | X | O | \n-------------\n| X | O | X | \n-------------\n"
+
+        # Перехват реального результата
+        captured_output = StringIO()
+        with mock.patch("sys.stdout", captured_output):
+            print_board(board)
+
+        # Сравнение результатов
+        self.assertEqual(captured_output.getvalue(), expected_output)
+
+
+if __name__ == "__main__":
+    unittest.main()
